c06d4fd4859b37e675a6ab39e26ae0fba8ef9e76
Fima <loboda.fima@icloud.com>
2025-05-28 19:35:25 +0300

    Project Setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,47 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import parserTypescript from "@typescript-eslint/parser";
+import pluginTypescript from "@typescript-eslint/eslint-plugin";
+import pluginReactHooks from "eslint-plugin-react-hooks";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default [
+  ...compat.extends("standard", "next/core-web-vitals", "next/typescript", "prettier"),
 
-export default eslintConfig;
+  {
+    languageOptions: {
+      parser: parserTypescript,
+    },
+    plugins: {
+      "@typescript-eslint": pluginTypescript,
+      "react-hooks": pluginReactHooks,
+    },
+    rules: {
+      "no-undef": "off",
+      camelcase: "off",
+      "@typescript-eslint/no-require-imports": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal", ["parent", "sibling"], "index", "object"],
+          "newlines-between": "never",
+          pathGroups: [{ pattern: "@app/**", group: "external", position: "after" }],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
+    },
+  },
+];
